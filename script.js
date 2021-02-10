54a788b30b2bce543f0475cb688f0973b34dcb4d
Abhinav Gupta <abhinavg4141@gmail.com>
2021-02-10 17:05:08 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+let login_options = document.querySelector(".login-options");
+let login_btn = document.querySelector(".login");
+
+login_btn.addEventListener("click",function(){
+    login_options.classList.toggle("hide");
+})
